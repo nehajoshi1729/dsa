358f13341494bb58cca8e4f86e5f34b40a0b6aa3
Neha Joshi <nehasuhasjoshi@gmail.com>
2022-03-24 23:32:06 +0530

    SortColours two pointer sol

---
+++ src/twoPointer/SortColours.java
@@ -0,0 +1,45 @@
+package twoPointer;
+
+/**
+ * Input: nums = [2,0,2,1,1,0]
+ * Output: [0,0,1,1,2,2]
+ * <p>
+ * follow-up : using library sort is not allowed
+ * <p>
+ * Dutch National Flag Algorithm, TC = O(N)
+ */
+
+public class SortColours {
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void sortColors(int[] nums) {
+        int low = -1;
+        int mid = 0;
+        int high = nums.length;
+
+        while (mid < high) {
+            if (nums[mid] == 1) {
+                mid++;
+            } else if (nums[mid] == 0) {
+                low++;
+                swap(nums, low, mid);
+                mid++;
+            } else {
+                high--;
+                swap(nums, high, mid);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 0, 2, 1, 1, 0};
+        sortColors(nums);
+        for (int i = 0; i < nums.length; i++)
+            System.out.println(nums[i]);
+
+    }
+}
