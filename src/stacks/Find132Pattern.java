11b5345880a7714f358233de34e79abbe77d0d79
Neha Joshi <nehasuhasjoshi@gmail.com>
2022-05-10 00:17:50 +0530

    added solution comments

--- src/stacks/Find132Pattern.java
+++ src/stacks/Find132Pattern.java
@@ -5,6 +5,14 @@ import java.util.Stack;
 /**
  * Given an array of n integers nums, a 132 pattern is a subsequence of three integers nums[i], nums[j] and nums[k]
  * such that i < j < k and nums[i] < nums[k] < nums[j].
+ *
+ * -- > monotonic stack
+ *          lower value -- max --- second max
+ *          iterate from right to left
+ *              1. running variable for second max (max of pop'ed elements from the stack)
+ *              2 .max will be stored in the stack (non max will get pop)
+ *              3. 32 structure is maintained by 1 & 2 steps
+ *              4. if the current element, while iterating is lesser than the second max. That's it.That's our 1
  */
 
 public class Find132Pattern {
