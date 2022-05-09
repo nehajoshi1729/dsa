e7a626d3c6bfb304450e2f1931952d6d79777bc5
Neha Joshi <nehasuhasjoshi@gmail.com>
2022-05-10 00:20:10 +0530

    minor refactor

--- src/stacks/Find132Pattern.java
+++ src/stacks/Find132Pattern.java
@@ -18,15 +18,15 @@ import java.util.Stack;
 public class Find132Pattern {
     public boolean find132pattern(int[] nums) {
         Stack<Integer> stack = new Stack<>();
-        int thirdElement = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
 
         for(int i = nums.length-1;i>=0;i--)
         {
-            if(nums[i] < thirdElement)
+            if(nums[i] < secondMax)
                 return true;
             while(!stack.isEmpty() && stack.peek() <nums[i])
             {
-                thirdElement = stack.pop();
+                secondMax = Math.max(secondMax,stack.pop());
             }
             stack.push(nums[i]);
         }
