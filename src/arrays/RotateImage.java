3dae315a630bc6434e6356196ce50a94e86b1cf2
Neha Joshi <nehasuhasjoshi@gmail.com>
2022-08-30 21:51:03 +0530

    2d arrays | rotate arrays

---
+++ src/arrays/RotateImage.java
@@ -0,0 +1,31 @@
+package arrays;
+
+public class RotateImage {
+    private void transpose(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+    }
+
+    private void reverseRows(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
+        }
+
+    }
+
+    public void rotate(int[][] matrix) {
+        transpose(matrix);
+        reverseRows(matrix);
+
+    }
+}
