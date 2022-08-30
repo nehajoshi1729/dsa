9720e127006822da12d3e4f06e3d35caf9a9828b
Neha Joshi <nehasuhasjoshi@gmail.com>
2022-08-30 21:54:05 +0530

    rotate arrays approach comments

--- src/arrays/RotateImage.java
+++ src/arrays/RotateImage.java
@@ -1,5 +1,16 @@
 package arrays;
 
+/**
+ * transpose and then reverse row wise
+ *      transpose loop DRY for a 3*3, swap symmetrically across diagonal
+ *      (0,0),(0,1),(0,2)
+ *      (1,1),(1,2)
+ *      (2,2)
+ *
+ *      reverse row wise using two pointers swap
+ *
+ */
+
 public class RotateImage {
     private void transpose(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
